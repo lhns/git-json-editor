d95cd34b8f7648918d4bcdd80a4a5004beb216e9
Pierre Kisters <pierrekisters@gmail.com>
2023-02-14 17:08:27 +0100

    git clone schema

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import './style.css'
 //import { setupCounter } from './counter'
 // @ts-ignore
 import { JSONEditor } from '@json-editor/json-editor/dist/jsoneditor.js'
+
+import { Buffer } from 'buffer'
+globalThis.Buffer = Buffer
+
+import git from 'isomorphic-git'
+import http from 'isomorphic-git/http/web'
+import LightningFS from '@isomorphic-git/lightning-fs';
+
 /*
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -24,11 +32,33 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 */
 const element = document.getElementById('app');
 
-const editor = new JSONEditor(element, {
-  theme: 'bootstrap5',
-  iconlib: 'openiconic',
-  schema: {}
-});
+const fs = new LightningFS('fs', {wipe: true})
+
+const dir = '/'
+git.clone({
+  fs,
+  http,
+  dir,
+  url: 'https://github.com/json-editor/json-editor',
+  corsProxy: 'https://cors.isomorphic-git.org',
+  singleBranch: true,
+  depth: 1,
+  onProgress: e => {console.log(e)}
+}).then(e => {
+  console.log(e)
+  console.log(fs)
+  console.log(fs.promises.readdir(dir).then(console.log))
+  fs.promises.readFile('/tests/fixtures/basic_person.json', {encoding: 'utf8'}).then(schemaString => {
+    console.log(schemaString)
+    
+    const editor = new JSONEditor(element, {
+      theme: 'bootstrap5',
+      iconlib: 'openiconic',
+      schema: JSON.parse(schemaString)
+    });
+  })
+})
+
 
 
 //setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
