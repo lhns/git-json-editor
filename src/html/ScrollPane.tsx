41f367acbb0f67e966214cf04ea053fb83d7ebcb
Pierre Kisters <pierrekisters@gmail.com>
2023-02-24 17:28:30 +0100

    fix horizontal scrolling of ScrollPane

--- src/html/ScrollPane.tsx
+++ src/html/ScrollPane.tsx
@@ -5,9 +5,11 @@ class ScrollPane extends React.Component<{ children: React.ReactNode }> {
         const {children} = this.props
         return <div className="h-100 d-flex">
             <div className="flex-fill h-100 d-flex flex-column">
-                <div className="flex-fill h-0 d-flex">
-                    <div className="flex-fill overflow-auto">
-                        {children}
+                <div className="flex-fill h-0 d-flex flex-row">
+                    <div className="flex-fill w-0 d-flex">
+                        <div className="flex-fill overflow-auto">
+                            {children}
+                        </div>
                     </div>
                 </div>
             </div>
